4e506d91f645344dc000067603aeb96b4d3242dc
keneskae <70793459+keneskae@users.noreply.github.com>
2020-09-05 16:32:33 +1000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $(".kenkeiso").lettering();
+  var fontColour = ["lightCoral","orangeRed","#ffccff","#17cfcf","goldenRod","lavender","#ccf5a3","chocolate","#009B77","cadetBlue","gold","mediumSlateBlue","seaGreen","antiqueWhite"];
+  var backgroundColour = ["darkKhaki","navajoWhite","steelBlue","#B565A7","oliveDrab","LightSeaGreen","#7575a3","paleGoldenRod","#EFC050","conflowerBlue","royalBlue","rosyBrown","#9B2335","sienna"];
+  var randColour = Math.floor(Math.random() * 13)
+  document.body.style.backgroundColor = backgroundColour[randColour];
+  document.getElementsByClassName("kenkeiso")[0].childNodes.forEach(function(element)
+  {
+	element.style.color = fontColour[randColour];
+  });
+
+});
+
+document.getElementsByClassName("button")[0].style.color = fontColour[randColour];
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
